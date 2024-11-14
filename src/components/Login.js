4191112c20b330dea1597145bdf25013d09d0112
xorms571 <xorms6865@naver.com>
2024-11-14 23:13:43 +0900

    final2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../AuthContext";
+import { useAuth } from "../utils/AuthContext";
 
 const Login = () => {
   const [username, setUsername] = useState("");
